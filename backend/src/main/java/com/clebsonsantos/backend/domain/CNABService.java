a4ce2bee22b62c8585d3db0c9e0ad065498d223a
Clebson Santos <clebsonsantosmtt@gmail.com>
2023-12-13 21:18:59 -0300

    chore: add controller to read file CNAB and proccess it

--- backend/src/main/java/com/clebsonsantos/backend/domain/CNABService.java
+++ backend/src/main/java/com/clebsonsantos/backend/domain/CNABService.java
@@ -3,6 +3,10 @@ package com.clebsonsantos.backend.domain;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -11,14 +15,27 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class CNABService {
   private final Path fileStorageLocation;
+  private final JobLauncher jobLauncher;
+  private final Job job;
 
-  public CNABService(@Value("${file.upload-dir}") String fileUploadDir) {
+  public CNABService(@Value("${file.upload-dir}") String fileUploadDir,
+      @Qualifier("jobLauncherAsync") JobLauncher jobLauncher,
+      Job job) {
     this.fileStorageLocation = Paths.get(fileUploadDir);
+    this.jobLauncher = jobLauncher;
+    this.job = job;
   }
 
   public void uploadCNABFile(MultipartFile file) throws Exception {
     var fileName = StringUtils.cleanPath(file.getOriginalFilename());
     var targetLocation = fileStorageLocation.resolve(fileName);
     file.transferTo(targetLocation);
+
+    var jobParameters = new JobParametersBuilder()
+        .addJobParameter("cnab", file.getOriginalFilename(), String.class, true)
+        .addJobParameter("cnbaFile", "file:" + targetLocation.toString(), String.class)
+        .toJobParameters();
+
+    jobLauncher.run(job, jobParameters);
   }
 }
